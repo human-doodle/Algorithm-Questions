a78ef6ad4a985ba5e164f54000a639f6c540c4ce
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2023-10-20 20:46:20 -0400

    Time: 34 ms (88.54%) | Memory: 16.4 MB (27.92%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,17 +1,26 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = {len(s) : 1}
-        def recursive(i):
-            if  i in dp:
+        dp = {len(s):1}
+
+        def rec(i):
+            if i in dp:
                 return dp[i]
+
             if s[i] == '0':
                 return 0
             
-            res = recursive(i+1)
-            if i+1 < len(s) and int(s[i:i + 2]) <= 26:
-                res+=recursive(i+2)
+            res = rec(i+1)
+            if i+2 <= len(s) and int(s[i:i+2]) <= 26:
+                res+=rec(i+2)
             dp[i] = res
             return res
-        if not s:
-            return 0
-        return recursive(0)
+        return rec(0)
+    
+
+    #  121
+    # /.   \.  
+    # 1     12 
+    # / \.  /
+    # 2. 21 1
+    # /.   
+    # 1    
