594c9bd19b4f8ba022dfe9daa82d6ec38db93d65
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2023-10-29 13:31:57 -0400

    Time: 71 ms (69.30%) | Memory: 23 MB (51.82%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,15 +1,25 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def count_ones(num):
-            count = 0
-            while num > 0:
-                count += num & 1
-                num >>= 1
-            return count
 
-        result = []
-        for i in range(n + 1):
-            result.append(count_ones(i))
-        return result
+        # dp O(n)
+        dp = [0]*(n+1)
+
+        for i in range(1, n+1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        
+        return dp
+
+        # # O(nlogn) because n is represented by logn bits in binary
+        # def count_ones(num):
+        #     count = 0
+        #     while num > 0:
+        #         count += num & 1
+        #         num >>= 1
+        #     return count
+
+        # result = []
+        # for i in range(n + 1):
+        #     result.append(count_ones(i))
+        # return result
 
         
