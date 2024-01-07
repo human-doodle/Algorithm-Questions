2435ae3225e2637db442bac282b6e6dfc797d621
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-01-07 18:18:28 +0530

    Time: 468 ms (93.38%) | Memory: 31.2 MB (47.12%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,11 +1,29 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # brute force using dictioanry
-        d = dict()
-        for num in nums:
-            if num in d:
-                return num
-            d[num] = 1
+        # brute force using dictioanry using non constant space
+        # d = dict()
+        # for num in nums:
+        #     if num in d:
+        #         return num
+        #     d[num] = 1
         
+        # constant space...
+        # must knwo: Floyd's circle detection algorithm and imagine list as linked list if unique numbers
+
+        slow, fast = 0,0
+        while True:
+            slow = nums[slow]
+            fast  = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow        
+
+
+
 
         
