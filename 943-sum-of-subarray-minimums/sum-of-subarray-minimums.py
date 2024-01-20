89304838e3aa8ed98c3fa5ed0ab8d5e86f4cda97
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-01-20 17:17:34 +0530

    Time: 327 ms (89.45%) | Memory: 21.4 MB (63.52%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -0,0 +1,35 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        arr.append(0)
+        stack = [] # The monotonic stack to track the nearest smaller element to the left for each element in the array.
+        result = 0
+
+        for i in range(len(arr)):
+            while stack and arr[i]<arr[stack[-1]]:
+                # Pop the top element from the stack
+                index = stack.pop()
+                # Calculate the contribution of the popped element to the sum
+                left = stack[-1] if stack else -1
+                result = (result + arr[index] * (i - index) * (index - left)) % MOD
+            # Push the current index onto the stack
+            stack.append(i)
+        
+        return result
+        
+
+        # The left boundary is determined by the element at the top of the stack before popping, and the right boundary is the current index.
+
+            # # brute force O(n^2) : tle
+            # MOD = 10**9 + 7
+            # result = 0
+            
+            # # Generate all subarrays
+            # for start in range(len(arr)):
+            #     for end in range(start, len(arr)):
+            #         # Find the minimum in the current subarray
+            #         min_val = min(arr[start:end + 1])
+            #         # Update the result with the contribution of the minimum value
+            #         result = (result + min_val) % MOD
+            # return result
+
