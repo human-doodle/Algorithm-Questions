d508c739ed4d78f6c8d534c0d5ff1df140c6981d
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-02-05 16:32:25 -0500

    Time: 51 ms (51.62%) | Memory: 17.5 MB (79.96%) - LeetSync

--- 1287-distance-between-bus-stops/distance-between-bus-stops.py
+++ 1287-distance-between-bus-stops/distance-between-bus-stops.py
@@ -1,13 +1,21 @@
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        if start> destination:
-            start, destination = destination, start #doesnt make a difference
-        pathsum, totalsum = 0,0
-        for i in range(len(distance)):
-            if i>=start and i<destination:
-                pathsum += distance[i]
-            totalsum += distance[i]
-        return min(pathsum, totalsum-pathsum) # totalsum-pathsum is the distance got the other way
+        # if start> destination:
+        #     start, destination = destination, start #doesnt make a difference
+        # pathsum, totalsum = 0,0
+        # for i in range(len(distance)):
+        #     if i>=start and i<destination:
+        #         pathsum += distance[i]
+        #     totalsum += distance[i]
+        # return min(pathsum, totalsum-pathsum) # totalsum-pathsum is the distance got the other way
+
+        # much elegant sol
+        a, b = min(start, destination), max(start, destination)
+
+        clockwise_dist = sum(distance[a:b])
+        counterclockwise_dist = sum(distance) - clockwise_dist
+
+        return min(clockwise_dist, counterclockwise_dist)
         
         
         
