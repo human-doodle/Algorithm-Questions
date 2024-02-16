9e6d7903d15617861c63084f0fe47196d93690ea
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-02-16 16:33:51 -0500

    Time: 344 ms (64.53%) | Memory: 36.9 MB (38.54%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        d = dict()
+        for num in arr:
+            d[num] = d.get(num, 0) + 1
+        
+        d = [v for k, v in sorted(d.items(), key=lambda item: item[1])]
+        print(d)
+        idx = 0
+        while k>0 and idx<len(d):
+            if d[idx]>=k:
+                d[idx] = d[idx]-k
+                k = 0
+            else:
+                balance = k-d[idx]
+                d[idx] = 0
+                k = balance
+            idx+=1
+        
+        res= 0
+        for i in d:
+            if i !=0:
+                res+=1
+
+        return res
+
+
+            
