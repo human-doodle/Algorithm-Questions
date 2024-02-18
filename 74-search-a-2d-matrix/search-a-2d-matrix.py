d2ec348534169f8f28edf0a473a4818cab0dfb86
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-02-17 20:28:08 -0500

    Time: 52 ms (26.14%) | Memory: 17.1 MB (88.37%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -4,26 +4,42 @@ class Solution:
         M = len(matrix)
         N = len(matrix[0])
 
-        # better:  find row and then binary search O(M + logN); O(1)
-        def bs(arr, target):
-            n = len(arr)
+        # optimal solution
+        def bs(matrix, target, M, N):
+            n = M*N
             low, high = 0, n-1
             while low <= high:
                 mid = ( high + low )//2
-                if arr[mid] == target:
+                if matrix[mid//N][mid%N] == target:
                     return True
-                elif target > arr[mid]:
+                elif target > matrix[mid//N][mid%N]:
                     low = mid + 1
                 else:
                     high = mid - 1
-                
             return False
-        
-        for i in range(M):
-            if matrix[i][0]<=target and matrix[i][N-1]>=target:
-                return bs(matrix[i], target)
 
-        return False
+        return bs(matrix, target, M, N)
+
+
+        # better:  find row and then binary search O(M + logN); O(1)
+        # def bs(arr, target):
+        #     n = len(arr)
+        #     low, high = 0, n-1
+        #     while low <= high:
+        #         mid = ( high + low )//2
+        #         if arr[mid] == target:
+        #             return True
+        #         elif target > arr[mid]:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+        #     return False
+        
+        # for i in range(M):
+        #     if matrix[i][0]<=target and matrix[i][N-1]>=target:
+        #         return bs(matrix[i], target)
+
+        # return False
 
         # brute force : O(M*N); O(1)
         
