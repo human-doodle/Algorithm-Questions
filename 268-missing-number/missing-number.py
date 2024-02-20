a10958c42e55ad03c7cb6bc27a881d5a0bde241c
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-02-19 20:12:46 -0500

    Time: 110 ms (72.62%) | Memory: 17.8 MB (64.04%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,27 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # O(3n) -> O(n) SC: O(n)
-        d = {}
-        for i in range(len(nums)+1):
-            d[i] = 0
-        for n in nums:
-            d[n] = 1
-        for k in d:
-            if d[k] == 0:
-                return k
-
-        # TC O(n) SC(1) : problem can overflow since we are adding
         n = len(nums)
-        # sum = n * (n+1)/2
-        # sum_nums = sum(nums)
-        # return sum - sum_nums
-
-        # O(n) O(1) : XOR bit manipulation
-        res = 0
-        for i in nums:
-            res^=i
-        return res
-
-
-
+        sum_n = (n*(n+1))//2
+        sum_nums  = 0
+        for num in nums:
+            sum_nums+=num
+        return sum_n - sum_nums
         
