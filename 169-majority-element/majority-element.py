6ac1d2111837e1f8191c92c25c1cc6434c90661c
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-02-21 11:59:13 -0500

    Time: 175 ms (9.05%) | Memory: 17.9 MB (83.55%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,37 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n = len(nums)
-        d = dict()
+
+        # Optimal solution, optimise the space: moore's voting algorithm, O(2N); O(1)
+        element = nums[0]
+        count = 1
+
+        for num in nums[1:]:
+            if count == 0:
+                element = num
+                count += 1
+                continue
+            if num != element:
+                count-=1
+            else:
+                count+=1
+        
+        # verify; although in this case, it is guaranteed
+        count = 0
         for num in nums:
-            d[num] = d.get(num, 0) + 1
-            if d[num] > n/2:
-                return num
+            if num == element:
+                count+=1
+        if count>n/2:
+            return element
+        return -1
+
+        # Brute force
+        # n = len(nums)
+        # d = dict()
+        # for num in nums:
+        #     d[num] = d.get(num, 0) + 1
+        #     if d[num] > n/2:
+        #         return num
+        
     # O(n) O(n)
                     
