ffdc42448b02c284d69c59fee456947d44e1602b
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-03-01 14:37:59 -0500

    Time: 31 ms (84.84%) | Memory: 16.7 MB (39.56%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,32 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # row = [1] * n
-
-        # for i in range(m-1):
-        #     newrow = [1] * n
-        #     for j in range(n-2, -1, -1):
-        #         newrow[j] = newrow[j+1] + row[j]
-        #     row = newrow
-        # return row[0]
-
-        # o(n+m) o(n)
-
-        # recursion
-        def check(i, j):
-            if i<0 or j<0 or i>=m or j>=n:
-                return False
-            return True
-        @cache
-        def helper(i,j):
-            if not check(i, j):
-                return 0
-            else:
-                if j==n-1 and i==m-1:
-                    return 1
-                else:
-                    # move right and down
-                    return helper(i+1,j)+helper(i, j+1)
-        return helper(0,0)
-
-        # TLE without cache
+        def countPaths(dp, i:int, j:int, m:int, n:int):
+            if i == n-1 and j == m-1: return 1
+            if i >= n or j >= m: return 0
+            if dp[i][j]!=-1: return dp[i][j]
+            else: 
+                dp[i][j] = countPaths(dp, i+1, j, m, n) + countPaths(dp, i, j+1, m, n)
+                print(dp[i][j])
+                return dp[i][j] 
+        dp = [[-1 for _ in range(m)] for _ in range(n)]
+        print(dp)
+        return countPaths(dp, 0, 0, m, n)
         
