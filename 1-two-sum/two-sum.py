4f81582e5d932606fdd65bfd71727d450b2a9645
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-05-07 22:21:50 -0400

    Time: 32 ms (92.57%) | Memory: 12.5 MB (77.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,12 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # hashing (O(n);O(n))
+        d = dict()
         for i, num in enumerate(nums):
             n = target - num
-            if n in nums:
-                j = nums.index(n)
-                if j!=i:
-                    return [i,j]
+            if n in d:
+                return [i, d[n]]
+            d[num] = i
+            
+        # python in O(n),O(1)
+        # for i, num in enumerate(nums):
+        #     n = target - num
+        #     if n in nums:
+        #         j = nums.index(n)
+        #         if j!=i:
+        #             return [i,j]
+        # double for loop
         # for i, num in enumerate(nums):
         #     n = target - num
         #     for j, num2 in enumerate(nums):
