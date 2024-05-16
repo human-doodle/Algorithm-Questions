5010f5748eedcc4d9ed73310e0dd825f89bbf533
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-05-16 19:44:30 -0400

    Time: 46 ms (63.74%) | Memory: 16.8 MB (92.06%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
@@ -4,15 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def postorder(self, root):
-        if root.val == 0 or root.val == 1:
-            return root.val
-        elif root.val == 2:
-            return self.postorder(root.left) or self.postorder(root.right)
-        elif root.val == 3:
-            return self.postorder(root.left) and self.postorder(root.right)
+class Solution:  
+    def postorder(self,node):
+        if node.val == 0 or node.val == 1:
+            return node.val 
+        if node.val == 2:
+            return self.postorder(node.left) or self.postorder(node.right)
+        elif node.val == 3:
+            return self.postorder(node.left) and self.postorder(node.right)
         return False
         
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
         return self.postorder(root)
+
+
+'''
+Input: root = [2,1,3,null,null,0,1]
+Output: true
+
+T, F, T, AND, OR
+
+
+'''
