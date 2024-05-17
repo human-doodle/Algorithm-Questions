5b3624cb6deaaabd7142788ede1fbb1efb5880aa
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-05-17 02:12:44 -0400

    Time: 44 ms (68.14%) | Memory: 17 MB (80.39%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -5,31 +5,48 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        if not root:
-            return root
-        # iterative
+
+    # recursive
+    def postorder(self, node, target):
+        if not node:
+            return
         
-        stack = [(root, None, False)]
+        node.left = self.postorder(node.left, target)
+        node.right = self.postorder(node.right, target)
+
+        if node.val == target and not node.right and not node.left:
+            return None
+        
+        return node
+
+        
+
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        
+        return self.postorder(root,target)
+
+        # # iterative
+        
+        # stack = [(root, None, False)]
 
 
-        while stack:
-            curr, parent, visited = stack.pop()
-            if curr:
-                if visited:
-                    if curr.val == target and not curr.right and not curr.left:
-                        if root == curr:
-                            del root
-                            return 
-                        if parent.right == curr:
-                            parent.right = None
-                        else:
-                            parent.left = None
-                        del curr
-                else:
-                    stack.append((curr, parent, True))
-                    stack.append((curr.right, curr, False))
-                    stack.append((curr.left, curr, False))
+        # while stack:
+        #     curr, parent, visited = stack.pop()
+        #     if curr:
+        #         if visited:
+        #             if curr.val == target and not curr.right and not curr.left:
+        #                 if root == curr:
+        #                     del root
+        #                     return 
+        #                 if parent.right == curr:
+        #                     parent.right = None
+        #                 else:
+        #                     parent.left = None
+        #                 del curr
+        #         else:
+        #             stack.append((curr, parent, True))
+        #             stack.append((curr.right, curr, False))
+        #             stack.append((curr.left, curr, False))
             
-        return root
+        # return root
 
