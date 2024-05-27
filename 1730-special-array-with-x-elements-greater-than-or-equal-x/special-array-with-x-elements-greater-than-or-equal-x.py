8422df5bde6bcf38ba828d681626acd71f0234da
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-05-27 01:51:45 -0400

    Time: 43 ms (43.57%) | Memory: 16.5 MB (86.58%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,14 +1,31 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        # O(n^2); O(1)
-        for i in range(len(nums)+1):
+        # sort and binary 
+        nums.sort()
+        left, right = 0, len(nums)
+        while(left<=right):
+            mid = left+(right-left)//2
             count = 0
-            for num in  nums:
-                if num>=i:
+            for num in nums:
+                if num>=mid:
                     count+=1
-            if count == i:
-                return i
+            if mid == count:
+                return mid
+            elif mid>count:
+                right = mid-1
+            else:
+                left = mid+1
         return -1
+
+        # # O(n^2); O(1)
+        # for i in range(len(nums)+1):
+        #     count = 0
+        #     for num in  nums:
+        #         if num>=i:
+        #             count+=1
+        #     if count == i:
+        #         return i
+        # return -1
 
             
         
