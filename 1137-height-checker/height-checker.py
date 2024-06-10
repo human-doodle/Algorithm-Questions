175e2e6c4fee43b310ec68237e906214ede6589d
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-06-10 00:30:12 -0400

    Time: 36 ms (77.24%) | Memory: 16.4 MB (77.64%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,7 +1,16 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         count = 0
-        new = sorted(heights)
+        # new = sorted(heights)
+        # count - sort
+        new = []
+        freq = {}
+        for i in range(len(heights)):
+            freq[heights[i]] = freq.get(heights[i], 0) + 1
+        for i in range(1, 101):
+            if i in freq:
+                for j in range(freq[i]):
+                    new.append(i)
         for i in range(len(heights)):
             if heights[i]!=new[i]:
                 count+=1
