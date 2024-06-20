7110063696f9020c9888ca437778edfd03676975
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-06-20 16:38:39 -0400

    Time: 1232 ms (11.85%) | Memory: 184.2 MB (18.03%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,10 +1,27 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0]*(len(text1)+1) for i in range(len(text2)+1)]
-        for i in range(len(text2)-1, -1, -1):
-            for j in range(len(text1)-1, -1, -1):
-                if text2[i] == text1[j]:
-                    dp[i][j]=1 + dp[i+1][j+1]
-                else:
-                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
-        return dp[0][0]
+        # dp = [[0]*(len(text1)+1) for i in range(len(text2)+1)]
+        # for i in range(len(text2)-1, -1, -1):
+        #     for j in range(len(text1)-1, -1, -1):
+        #         if text2[i] == text1[j]:
+        #             dp[i][j]=1 + dp[i+1][j+1]
+        #         else:
+        #             dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        # return dp[0][0]
+
+        def _longestCommonSubsequence(text1, text2, s1, s2, memo):
+            key = (s1, s2)
+            if key in memo:
+                return memo[key]
+            if s1 >=len(text1) or s2>=len(text2):
+                return 0
+            if text1[s1] == text2[s2]:
+                memo[key] =  1 + _longestCommonSubsequence(text1, text2, s1+1, s2+1, memo)
+                return memo[key]
+            else:
+                memo[key] = max(
+                    _longestCommonSubsequence(text1, text2, s1+1, s2, memo),
+                    _longestCommonSubsequence(text1, text2, s1, s2+1, memo))
+                return memo[key]
+                
+        return _longestCommonSubsequence(text1, text2, 0, 0, {})
