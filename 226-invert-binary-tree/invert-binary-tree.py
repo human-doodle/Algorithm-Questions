987c0fc4e7060d18881a01e9c3f4785f22e07c9c
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-06-21 18:58:23 -0400

    Time: 38 ms (38.70%) | Memory: 16.4 MB (68.80%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -6,23 +6,35 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return 
-        q = collections.deque()
-        q.append(root)
-
-        while q:
-            curr = q.popleft()
-
-            if curr.left:
-                q.append(curr.left) 
-
-            if curr.right :
-                q.append(curr.right) 
-
-            curr.left, curr.right = curr.right, curr.left
-            
+        def _flip_tree(root):
+            if not root:
+                return 
+            if not root.right and not root.left:
+                root.right, root.left = root.left, root.right
+            root.right, root.left = root.left, root.right
+            _flip_tree(root.left)
+            _flip_tree(root.right)
+        
+        _flip_tree(root)
         return root
+        
+        # if not root:
+        #     return 
+        # q = collections.deque()
+        # q.append(root)
+
+        # while q:
+        #     curr = q.popleft()
+
+        #     if curr.left:
+        #         q.append(curr.left) 
+
+        #     if curr.right :
+        #         q.append(curr.right) 
+
+        #     curr.left, curr.right = curr.right, curr.left
+            
+        # return root
 
                 
                 
