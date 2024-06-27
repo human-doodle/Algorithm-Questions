afd22a5fe243ce40ddc67c8a6d9a7a8ce95c413f
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-06-27 00:12:18 -0400

    Time: 31 ms (81.78%) | Memory: 16.7 MB (8.12%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -3,6 +3,7 @@ class Solution:
         i, j = 0, 0
         res = []
         count = 0
+
         while i<len(word1) and j<len(word2):
             if count % 2 == 0:
                 res.append(word1[i])
@@ -12,9 +13,11 @@ class Solution:
                 res.append(word2[j])
                 j+=1
                 count+=1
+
         if i<len(word1):
             res.append(word1[i:])
         if j<len(word2):
             res.append(word2[j:])
+            
         return ''.join(res)
             
