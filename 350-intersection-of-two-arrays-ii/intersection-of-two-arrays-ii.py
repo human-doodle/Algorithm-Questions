60928734206e8f7232f6cf74f02ad1845c2627c9
Shivani Pal <46643099+human-doodle@users.noreply.github.com>
2024-07-02 15:57:00 -0400

    Time: 45 ms (77.14%) | Memory: 16.7 MB (30.84%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d1 = {}
+        d2 = {}
+        res = []
+        for i in nums1:
+            d1[i] = d1.get(i, 0) + 1
+        for i in nums2:
+            d2[i] = d2.get(i, 0) + 1
+        for k, v in d2.items():
+            if k in d1:
+                arr = [k]*min(v, d1[k])
+                res = res + arr
+        return res
+
+
